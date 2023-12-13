b6d3c442ea6d8592704974804bdda139987849ec
eduardoserpa1 <dudu.rserpa@gmail.com>
2023-12-13 17:13:30 -0300

    classes and objects

---
+++ SoundScreen/SoundScreen/Musica.cs
@@ -0,0 +1,22 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disposivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disposivel)
+        {
+            Console.WriteLine("Disponivel no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano plus");
+        }
+    }
+}
--- SoundScreen/SoundScreen/Program.cs
+++ SoundScreen/SoundScreen/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musica1 = new Musica();
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 273;
+musica1.disposivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disposivel = false;
+
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
